7fc45aad2a04fc8413e6ffb8b268be9b74640c30
kaleidoscope684 <kalel684@gmail.com>
2015-08-17 00:43:50 +0000

    Rewind app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rewind::Application.config.secret_token = '001690036dc4d958389dd79091ac5b18af2a95ac668007a335ce977f0f32258affeffb18a172091c89399d67d4b7bfa5fa9120d5829e981feef43f3167969297'
